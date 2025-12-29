14c305618b9a9fda65486526477431a44b5abce3
garlie0206 <garlie0206@gmail.com>
2025-12-29 20:01:14 +0000

    added more weather data

--- src/app.js
+++ src/app.js
@@ -2,9 +2,40 @@ function refreshWeather(response) {
   let tempratureElement = document.querySelector("temprature");
   let temprature = response.data.temprature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML =  formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = '${response.data.temprature.humidty}%';
+  windSpeedElement.innerHTML = '${response.data.wind.speed}'km/h;
   tempratureElement.innerHTML = Math.round(temprature);
+}
+
+function formatDate(date) {
+    let day = date.getDay();
+    let minutes = date.getMinutes();
+    let hours = date.gethours();
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"];
+
+        let day = days[date.getDay()];
+
+        if (minutes <10) {
+            minutes = '0${minutes}';
+        }
+    return '${day} ${hours} ${minutes}';
 }
 
 function searchCity(city) {
