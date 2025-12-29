30c6f7e0979d23a418ced86c5685629855f68658
garlie0206 <garlie0206@gmail.com>
2025-12-29 20:22:18 +0000

    updated API

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "ob966ee5c00a195346b3f35b10dtd623";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
